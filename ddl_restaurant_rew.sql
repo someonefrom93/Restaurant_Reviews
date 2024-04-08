6f9717506f6161cb1e75b0838c9e490f1b3d18a9
Juan Diego A.H <j.diego93@outlook.com>
2024-04-07 21:24:15 -0600

    MISC categories ingestion

--- ddl_restaurant_rew.sql
+++ ddl_restaurant_rew.sql
@@ -7,3 +7,28 @@ CREATE TABLE IF NOT EXISTS restaurants_reviews(
     user_id text,
     "date" text
 );
+
+CREATE TABLE IF NOT EXISTS misc_categories(
+    gmap_id text,
+    Accessibility text[],
+    Activities text[],
+    Amenities text[],
+    Atmosphere text[],
+    Crowd text[],
+    Dining_options text[],
+    From_the_business text[],
+    Getting_here text[],
+    Health_and_safety text[],
+    Health_and_safety_1 text[],
+    Highlights text[],
+    Lodging_options text[],
+    Offerings text[],
+    Payments text[],
+    Planning text[],
+    Popular_for text[],
+    Recycling text[],
+    Service_options text[]
+    
+);
+
+DROP TABLE misc_categories;
